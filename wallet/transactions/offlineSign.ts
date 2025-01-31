fd785bf8572684e57f4fa1ca6bbd86360fbe1c7b
Thoralf-M <thoralf.mue@gmail.com>
2025-01-31 14:32:11 +0100

    feat(wallet): improve offlineSign example

--- wallet/transactions/offlineSign.ts
+++ wallet/transactions/offlineSign.ts
@@ -2,7 +2,7 @@
 
 import { IotaClient } from '@iota/iota-sdk/client';
 import { decodeIotaPrivateKey } from '@iota/iota-sdk/cryptography';
-import { Secp256k1Keypair } from '@iota/iota-sdk/keypairs/secp256k1';
+import { Ed25519Keypair } from '@iota/iota-sdk/keypairs/ed25519';
 import { Transaction } from '@iota/iota-sdk/transactions';
 
 const client = new IotaClient({
@@ -10,16 +10,16 @@ const client = new IotaClient({
 });
 
 (async () => {
-    const privateKey = "iotaprivkey1q8cy2ll8a0dmzzzwn9zavrug0qf47cyuj6k2r4r6rnjtpjhrdh52vpegd4f";
+    const privateKey = "iotaprivkey1qrqvh7ps8ugvecus2sncjm78xpw6uqx77a4khzeg70yys7umtn6dwc42e24";
     const decoded = decodeIotaPrivateKey(privateKey);
-    const keypair = Secp256k1Keypair.fromSecretKey(decoded.secretKey);
+    const keypair = Ed25519Keypair.fromSecretKey(decoded.secretKey);
     const senderAddress = keypair.getPublicKey().toIotaAddress()
     console.log(senderAddress)
 
     // Send 1 to the sender
-    let outputs = [{ address: senderAddress, amount: 1 }]
+    let transfers = [{ address: senderAddress, amount: 1 }]
 
-    let tx = new Transaction();
+    // Get inputs online
     const inputsObjectRefs = [];
     let inputs = (await client.getOwnedObjects({ owner: senderAddress })).data
     for (const input of inputs) {
@@ -30,15 +30,22 @@ const client = new IotaClient({
         });
     }
     console.log("Inputs: ", inputsObjectRefs)
-    for (const output of outputs) {
-        // Unefficient to split in a loop, one could just split multiple amounts at once
-        const coin = tx.splitCoins(tx.gas, [output.amount]);
-        tx.transferObjects([coin], output.address);
-    }
 
+    // Offline
+    let tx = new Transaction();
+    // first, split the gas coin into multiple coins
+    const coins = tx.splitCoins(
+        tx.gas,
+        transfers.map((transfer) => transfer.amount),
+    );
+    // next, create a transfer transaction for each coin
+    transfers.forEach((transfer, index) => {
+        tx.transferObjects([coins[index]], transfer.address);
+    });
+
+    tx.setGasPayment(inputsObjectRefs);
     tx.setGasBudget(100000000)
     tx.setGasPrice(1000)
-    tx.setGasPayment(inputsObjectRefs);
     tx.setSender(senderAddress)
     let transaction_bytes = await tx.build();
     console.log(transaction_bytes)
