300fe28204c3efafcff27ecad373d749897b7a9f
Thoralf Müller <thoralf.mue@gmail.com>
2025-06-10 11:56:18 +0200

    feat(graphql): add staked-objects example

---
+++ graphql/staked-objects.ts
@@ -0,0 +1,85 @@
+// ts-node graphql/staked-objects.ts
+
+import { GraphQLQueryOptions, GraphQLQueryResult, IotaGraphQLClient } from '@iota/iota-sdk/graphql';
+import { graphql } from '@iota/iota-sdk/graphql/schemas/2025.2';
+
+const STAKED_IOTA_TYPE = "0x3::staking_pool::StakedIota";
+// const TIMELOCKED_STAKED_IOTA_TYPE = "0x3::timelocked_staking::TimelockedStakedIota";
+
+(async () => {
+    const gqlClient = new IotaGraphQLClient({
+        url: 'https://graphql.testnet.iota.cafe',
+    });
+
+    let res = { stakeObjects: 0, addresses: new Set<string>() };
+
+    let cursorSection;
+    while (true) {
+        let query = `query getStakedIota($type: String, $cursor: String) {
+            objects(filter: {type: $type}, after: $cursor) {
+                nodes {
+                    address
+                    owner {
+                        ... on AddressOwner {
+                            owner {
+                                address
+                            }
+                        }
+                    }
+                }
+                pageInfo {
+                hasNextPage
+                endCursor
+                }
+            }
+        }`;
+
+        let result: GraphQLQueryResult = await queryGraphQl(
+            gqlClient,
+            query,
+            {
+                type: STAKED_IOTA_TYPE,
+                cursor: cursorSection
+            },
+        );
+
+        if (result.errors) {
+            throw new Error("GraphQL query failed: " + JSON.stringify(result.errors));
+            break;
+        }
+
+        // @ts-ignore
+        for (let node of result.data.objects.nodes) {
+            res.stakeObjects++;
+            console.log(node)
+            if (node.owner.owner) {
+                res.addresses.add(node.owner.owner.address)
+            } else {
+                console.warn("No owner found for node: ", node);
+            }
+        }
+
+        console.log(res.addresses.size, " addresses, ", res.stakeObjects, " stake objects");
+        // @ts-ignore
+        if (result.data.objects.pageInfo.hasNextPage) {
+            // @ts-ignore
+            cursorSection = result.data.objects.pageInfo.endCursor;
+            console.log("next cursor:" + cursorSection);
+        } else {
+            break;
+        }
+    }
+    console.log(res);
+})();
+
+async function queryGraphQl(
+    gqlClient: IotaGraphQLClient,
+    query: string,
+    variables: Record<string, any>,
+): Promise<GraphQLQueryResult> {
+    const options: GraphQLQueryOptions = {
+        query: graphql(query),
+        variables,
+    };
+    return gqlClient.query(options);
+}
